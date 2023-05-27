4a94739128e9a3f28911201c74d7872bd90f49fc
Anatoliy <pipxfox@gmail.com>
2023-05-27 16:19:24 +0300

    commit step 3

---
+++ src/cli.js
@@ -0,0 +1,11 @@
+import readlineSync from 'readline-sync';
+
+let name;
+
+function greetings() {
+  console.log('Welcome to the Brain Games!');
+  name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+}
+
+export default greetings;
