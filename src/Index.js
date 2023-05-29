3095c7a1406d04cb07a9b83463db4fee2552d779
Anatoliy <pipxfox@gmail.com>
2023-05-29 16:22:37 +0300

    fix 4

--- src/Index.js
+++ src/Index.js
@@ -1,21 +1,21 @@
 function randomNum(min, max) {
-    const x = Math.floor(Math.random() * (max - min + 1) + min);
-    return x;
+  const x = Math.floor(Math.random() * (max - min + 1) + min);
+  return x;
+}
+
+function getCondition(correctAnswer, userAnswer, name, counter) {
+  let bool = 'true';
+  console.log(correctAnswer, userAnswer);
+  if (correctAnswer === userAnswer) {
+    console.log('Correct!');
+  } else {
+    bool = 'false';
+    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'. Let's try again, ${name}!`);
   }
-  
-  function getCondition(correctAnswer, userAnswer, name, counter) {
-    let bool = 'true';
-    console.log(correctAnswer, userAnswer);
-    if (correctAnswer === userAnswer) {
-      console.log('Correct!');
-    } else {
-      bool = 'false';
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'. Let's try again, ${name}!`);
-    }
-    if (counter >= 3) {
-      console.log(`Congratulations, ${name}!`);
-    }
-    return bool;
+  if (counter >= 3) {
+    console.log(`Congratulations, ${name}!`);
   }
-  
-  export { randomNum, getCondition };
+  return bool;
+}
+
+export { randomNum, getCondition };
